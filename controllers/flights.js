41894b18d027263dc875d426605cb9e5c8c11148
RareTC <trevorjcampbell@aol.com>
2023-02-06 13:54:56 -0800

    New title, and add flight no. input

--- controllers/flights.js
+++ controllers/flights.js
@@ -12,23 +12,23 @@ function index(req, res) {
   });
 }
 
+function newFlight(req, res) {
+  res.render('flights/new');
+}
+
 function create(req, res) {
   // Convert nowShowing's checkbox to a boolean
   req.body.nowFlying = !!req.body.nowFlying;
   // Remove leading/trailing spaces
-  req.body.cast = req.body.cast.trim();
-  if (req.body.cast) req.body.cast = req.body.cast.split(/\s*,\s*/);
-  for (let key in req.body) {
-    if (req.body[key] === '') delete req.body[key];
-  }
+  // req.body.cast = req.body.cast.trim();
+  // if (req.body.cast) req.body.cast = req.body.cast.split(/\s*,\s*/);
+  // for (let key in req.body) {
+  //   if (req.body[key] === '') delete req.body[key];
+  // }
   const movie = new Flight(req.body);
   movie.save(function(err) {
     if (err) return res.redirect('/flights/new');
     console.log(flight);
     res.redirect('/flights');
   });
-}
-
-function newFlight(req, res) {
-  res.render('flights/new');
 }
